2b0f23a735d7d4ceacc0027795d5b5d9f4fe1587
Minosoa <151531869+minoravelonirina@users.noreply.github.com>
2024-09-10 10:05:24 +0300

    Update updatePossession.jsx

--- src/pages/update/updatePossession.jsx
+++ src/pages/update/updatePossession.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import './update.css';
-import base_url from '../../../baseUrl';
+import base_url from '../../baseUrl';
 
 export default function UpdatePossession() {
   const [newLibelle, setNewLibelle] = useState('');
